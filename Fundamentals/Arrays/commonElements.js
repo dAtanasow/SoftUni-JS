411bd07898bc40e622df398ca81373292051f63d
dAtanasow <denisatanasov76@gmail.com>
2023-09-28 14:33:51 +0300

    add exercises

---
+++ Fundamentals/Arrays/commonElements.js
@@ -0,0 +1,16 @@
+function elements(arr1, arr2) {
+    for (let el of arr1) {
+        for (let ele of arr2) {
+            if (el === ele) {
+                console.log(el);
+            }
+        }
+    }
+}
+
+
+elements(['Hey', 'hello', 2, 4, 'Peter', 'e'],
+    ['Petar', 10, 'hey', 4, 'hello', '2']
+)
+elements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],
+    ['s', 'o', 'c', 'i', 'a', 'l'])
---
+++ Fundamentals/Arrays/mergeArrays.js
@@ -0,0 +1,23 @@
+function merge(arr1, arr2) {
+    let result = [];
+    let counter = 0
+    for (let i = 0; i < arr1.length; i++) {
+        let el1 = arr1[i];
+        let el2 = arr2[i];
+        if (counter % 2 === 0) {
+            let sum = Number(el1) + Number(el2)
+            result.push(sum)
+        } else {
+            result.push(el1 + el2)
+        }
+        counter++
+    }
+
+    console.log(result.join(' - '));
+}
+
+merge(['5', '15', '23', '56', '35'],
+    ['17', '22', '87', '36', '11']
+)
+merge(['13', '12312', '5', '77', '4'],
+    ['22', '333', '5', '122', '44'])
