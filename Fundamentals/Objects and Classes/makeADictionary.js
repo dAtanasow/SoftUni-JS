1163726340e5aa8c45651bde056010612f47ec98
dAtanasow <denisatanasov76@gmail.com>
2023-11-10 10:39:06 +0200

    add

--- Fundamentals/Objects and Classes/makeADictionary.js
+++ Fundamentals/Objects and Classes/makeADictionary.js
@@ -1,8 +1,34 @@
+// You will receive an array with strings in the form of JSON's.
+
+// You have to parse these strings and combine them into one object. Every string from the array will hold terms and a description. If you receive the same term twice, replace it with the new definition.
+
+// Print every term and definition in that dictionary on new line in format:
+// `Term: ${term} => Definition: ${definition}`
+
+// Don't forget to sort the dictionary alphabetically by the terms as in real dictionaries.
+
+
+
 function dictonary(params) {
-    
+    let dict = {};
+
+    for (let el of arr) {
+        let obj = JSON.parse(el);
+        dict = Object.assign(dict, obj);      //merges multiple objects into one
+    }
+    let sortedKeys = Object.keys(dict);        //focus on the keys coffee, bus, boiler, tape.. 
+    sortedKeys.sort((a, b) => a.localeCompare(b));   // sort 
+
+    for (let term of sortedKeys) {
+        let def = dict[term];
+        console.log(`Term: ${term} => Definition: ${def}`);
+    }
 }
 
-dictonary([ '{"Coffee":"A hot drink made
+
+dictonary([
+
+    '{"Coffee":"A hot drink made
     
     from the roasted and ground
     
@@ -32,4 +58,14 @@ dictonary([ '{"Coffee":"A hot drink made
     
     or fasten something."}',
     
-    '{"Microphone":"An instrument  for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}']);
+    '{"Microphone":"An instrument
+    
+    for converting sound waves into
+    
+    electrical energy variations
+    
+    which may then be amplified,
+    
+    transmitted, or recorded."}'
+    
+    ]);
