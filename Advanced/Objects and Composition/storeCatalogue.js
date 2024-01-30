36d74cc0495b5c93d1ed6339fbb2ac276b08f975
dAtanasow <denisatanasov76@gmail.com>
2024-01-30 18:09:38 +0200

    add

--- Advanced/Objects and Composition/storeCatalogue.js
+++ Advanced/Objects and Composition/storeCatalogue.js
@@ -1,23 +1,39 @@
 function storeCatalog(arr) {
-    let result = {}
-    for (let tokens of arr) {
-        let [product, price] = tokens.split(' : ');
-        price = Number(price)
-        result[product] = price
+    let sortedArr = arr.sort((a, b) => a.localeCompare(b))
+    let obj = {}
+    for (let i = 0; i < sortedArr.length; i++) {
+        let element = sortedArr[i].split(' : ').join(': ');
+
+        let firstLetter = element[0]
+
+        if (obj[firstLetter] == undefined) {
+            obj[firstLetter] = [];
+        }
+
+        obj[firstLetter].push(` ${element}`)
     }
 
-    Object.keys(result).sort((a, b) => a.localeCompare(b))
+    let output = []
 
-    console.log(result);
+    for (let key in obj) {
+        if (Object.hasOwnProperty.call(obj, key)) {
+            let element = obj[key]
+            output = [...output, key, ...element]
+        }
+    }
+
+    //console.log(output.join('\n'));
+    return output.join('\n')
+
 }
 
 storeCatalog(['Appricot : 20.4',
-'Fridge : 1500',
-'TV : 1499',
-'Deodorant : 10',
-'Boiler : 300',
-'Apple : 1.25',
-'Anti-Bug Spray : 15',
-'T-Shirt : 10']
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']
 
 );
